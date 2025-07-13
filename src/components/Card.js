f3cae37cb3d182c4f7764da595d67881d67a9278
arnaud <arnaud.dujardin76@gmail.com>
2025-07-13 12:22:47 +0200

    Ajout fetch axios, slice et map

---
+++ src/components/Card.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Card = ({ movie }) => {
+  return (
+    <div className="card">
+      <img
+        src={
+          movie.poster_path
+            ? "https://image.tmdb.org/t/p/original/" + movie.poster_path
+            : "./img/poster.jpg"
+        }
+        alt={`affiche ${movie.title}`}
+      />
+    </div>
+  );
+};
+
+export default Card;
